9c1e98808eeeb9b69c154c816f038df684aa1edc
Bruno Gutierrez <brunogutierrezgil@gmail.com>
2026-04-15 14:27:14 -0400

    script de creacion de DB

---
+++ dbCreate.sql
@@ -0,0 +1,13 @@
+-- Database: TBD
+
+-- DROP DATABASE IF EXISTS "TBD";
+
+CREATE DATABASE "TBD"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Chile.1252'
+    LC_CTYPE = 'Spanish_Chile.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
