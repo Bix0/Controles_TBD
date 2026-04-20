e3e12da8a9dbb0a9759a4703b4e13d94ec739fce
emiliopobleteleiva <emilio.poblete@usach.cl>
2026-04-19 20:17:47 -0400

    Se cambia el nombre de la carpeta postreTBD y se borran los archivos innecesarios

--- CONTROL 1/consulta_4.sql
+++
@@ -1,6 +0,0 @@
-select distinct c.Nombres, c.Apellido_Paterno, c.Apellido_Materno from Cliente c
-join Citas ci on c.Id_cliente = ci.Id_Cliente
-join Detalle_Cita dc on ci.Id_cita = dc.id_Cita
-where c.Genero = "hombre" and dc.Id_servicio in(1,2)
-group by c.Id_cliente, c.Nombres, c.Apellido_Paterno
-having count(Distinct dc.Id_servicio) = 2
--- CONTROL 1/consulta_5.sql
+++
@@ -1,8 +0,0 @@
-SELECT Cliente.Nombres, Cliente.Apellido_Paterno, Comuna.Nombre_Comuna, Peluqueria.Nombre, Detalle_Cita.Subtotal 
-FROM Cliente
-JOIN Comuna ON Cliente.Id_Comuna = Comuna.Id_Comuna
-JOIN Citas ON Cliente.Id_Cliente = Citas.Id_Cliente
-JOIN Peluqueria ON Citas.Id_Peluqueria = Peluqueria.Id_Peluqueria
-JOIN Detalle_Cita ON Citas.Id_Cita = Detalle_Cita.Id_Cita
-JOIN Servicio ON Detalle_Cita.Id_Servicio = Servicio.Id_Servicio
-WHERE Servicio.Nombre = 'Tinte';
